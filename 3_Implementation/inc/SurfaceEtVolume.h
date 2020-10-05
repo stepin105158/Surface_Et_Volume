75763d52b1f947ce3355cc6f0c3e70c8ccd9acf2
Karti A Nair <52342562+stepin105169@users.noreply.github.com>
2020-10-05 20:44:31 +0530

    Update SurfaceEtVolume.h

--- 3_Implementation/inc/SurfaceEtVolume.h
+++ 3_Implementation/inc/SurfaceEtVolume.h
@@ -1,17 +1,10 @@
 #ifndef SURFACEETVOLUME_H_INCLUDED
 #define SURFACEETVOLUME_H_INCLUDED
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
 
-void mainmenu();
-void sphere();
-void cone();
-void cylinder();
-void cube();
-void circle();
-void square();
-void rectangle();
-void triangle();
+float sphere(float radius);
+float cone(float radius, float slantheight);
 
 #endif // SURFACEETVOLUME_H_INCLUDED
