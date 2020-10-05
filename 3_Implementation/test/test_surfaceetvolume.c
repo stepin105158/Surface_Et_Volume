54b26c5bbb48ef0813ed4dabc0c1bc6dd5240492
Karti A Nair <52342562+stepin105169@users.noreply.github.com>
2020-10-05 20:52:26 +0530

    Update test_surfaceetvolume.c

--- 3_Implementation/test/test_surfaceetvolume.c
+++ 3_Implementation/test/test_surfaceetvolume.c
@@ -1,22 +1,52 @@
-#include "unity.h"
-//#include "SurfaceEtVolume.h"
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 
-	void setUp()
-	{	}
-	void tearDown()
-	{	}
+/* Modify these two lines according to the project */
+#include <SurfaceEtVolume.h>
+#define PROJECT_NAME    "Surface_Et_Volume"
 
-	void test_sphere(void)
-	{
-  		TEST_ASSERT_EQUAL(1, sphere(1));
-  		TEST_ASSERT_EQUAL(0, sphere(0));
-	}
+/* Prototypes for all the test functions */
+void test_sphere(void);
+void test_cone(void);
 
-	int test_main(void)
-	{
-	  	UNITY_BEGIN();
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "sphere", test_sphere);
+  CU_add_test(suite, "cone", test_cone);
 
-	  	RUN_TEST(test_sphere);
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
 
-  		return UNITY_END();
-	}
+/* Write all the test functions */ 
+void test_sphere(void) {
+  CU_ASSERT(113.040001 == sphere(3));
+  
+  /* Dummy fail*/
+  CU_ASSERT(15 == sphere(2));
+}
+
+void test_cone(void) {
+  CU_ASSERT(25.120001 == cone(2,2));
+  
+  /* Dummy fail*/
+  CU_ASSERT(30 == cone(1, 9));
+}
