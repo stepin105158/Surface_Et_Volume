d53f054c9e0a444652d7f222cc9044f38e90a512
Karti A Nair <52342562+stepin105169@users.noreply.github.com>
2020-10-05 20:42:30 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,47 +1,45 @@
-#include "SurfaceEtVolume.h"
+#include <SurfaceEtVolume.h>
 
-int main(){
+int main()
+{
     int n;
     int choose;
     int again = 1;
-
     while(again == 1){
-        mainmenu();
+        printf("\t\tTo get the area and volume of solid shapes\n\n\n");
+        printf("\t\t\tHere is your menu\n\n");
+        printf("\t\t\t3D SHAPES\n\n");
+        printf("\t\t\t  1. Sphere\n");
+        printf("\t\t\t  2. Cone\n");
+        printf("\t\t\t  3. Cylinder\n");
+        printf("\t\t\t  4. Cube\n\n");
+        printf("\t\t\t2D SHAPES\n\n");
+        printf("\t\t\t  5. Circle\n");
+        printf("\t\t\t  6. Square\n");
+        printf("\t\t\t  7. Rectangle\n");
+        printf("\t\t\t  8. Triangle\n\n");
+        printf("\t\t\tEnter your option\t");
         scanf("%d",&n);
         system("CLS");
     switch(n){
-    case 1:
+    case 1:                     //sphere
         system("CLS");
-        sphere();
+        float r;
+        printf("Enter the radius of sphere: ");
+        scanf("%f", &r);
+        printf("The area of sphere is %f\n\n",sphere(r));
         break;
-    case 2:
+    case 2:                     //cone
         system("CLS");
-        cone();
+        float ra, slant;
+        printf("Enter radius of cone: ");
+        scanf("%f", &ra);
+        printf("\nEnter the slant height of cone: ");
+        scanf("%f", &slant);
+        printf("The area of cone is %f\n\n",cone(ra,slant));
         break;
-    case 3:
-        system("CLS");
-        cylinder();
-        break;
-    case 4:
-        system("CLS");
-        cube();
-        break;
-    case 5:
-        system("CLS");
-        circle();
-        break;
-    case 6:
-        system("CLS");
-        square();
-        break;
-    case 7:
-        system("CLS");
-        rectangle();
-        break;
-    case 8:
-        system("CLS");
-        triangle();
-        break;
+
+
     default:
         printf("Error\n");
         break;
@@ -59,6 +57,5 @@ int main(){
             break;
         }
     }
-    test_main();
     return 0;
 }
